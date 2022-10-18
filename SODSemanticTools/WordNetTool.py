0c98f2faf86e6ca2f997a596fc7bfb3f33d8507d
MarKobe1990 <masiyuan268@outlook.com>
2022-10-18 17:52:21 +0800

    update tree

--- SODSemanticTools/WordNetTool.py
+++ SODSemanticTools/WordNetTool.py
@@ -4,7 +4,11 @@ from nltk.corpus.reader import Synset
 from treelib import Tree
 from collections import namedtuple
 
-Node = namedtuple('Node', ['count'])
+
+class TreeNodeData:
+    def __init__(self, count,files_path_list):
+        self.count = count,
+        self.files_path_list = files_path_list
 
 
 def get_root_name(input_word):
@@ -13,34 +17,41 @@ def get_root_name(input_word):
     return hypernyms_paths
 
 
-if __name__ == '__main__':
-    result_list = get_root_name("neck_brace")
-    if len(result_list) > 1:
-        length = len(result_list[0])
-        final_result = result_list[0]
-        for result in result_list:
+def build_tree(tree, paths_result_list, file_path):
+    # 选择最短路径
+    if len(paths_result_list) > 1:
+        length = len(paths_result_list[0])
+        final_result = paths_result_list[0]
+        for result in paths_result_list:
             if len(result) < length:
                 final_result = result
     else:
-        final_result = result_list
-    result_list2 = get_root_name("penis")
-    print(final_result[0].name().split('.')[0])
-    # print(result_list2)
-    # 建立树结构
-    tree = Tree()
+        final_result = paths_result_list[0]
+    # 父节点名称
+    father_node_id_for_next_node = ''
     for idx, element in enumerate(final_result):
         name = element.name().split('.')[0]
-        father_node_id_for_next_node = ''
         if not tree.contains(name):
-            if len(father_node_id_for_next_node) == 0:
-                tree.create_node(name, name, data=0)
+            # 为根节点
+            tree_node_data = TreeNodeData(count=int(1), files_path_list=[file_path])
+            if len(father_node_id_for_next_node) == 0 and idx == 0:
+                tree.create_node(tag=name, identifier=name, data=tree_node_data)
+                father_node_id_for_next_node = name
             else:
-                tree.create_node()
-            father_node_id_for_next_node = name
+                tree.create_node(tag=name, identifier=name, parent=father_node_id_for_next_node,
+                                 data=tree_node_data)
+                father_node_id_for_next_node = name
         else:
-            tree.get_node(name).data = tree.get_node(name).data + 1
+            node_data = tree.get_node(nid=name).data
+            tree_node_data = TreeNodeData(count=node_data.count[0] + 1, files_path_list=node_data.files_path_list.append(file_path))
+            tree.update_node(nid=name, data=tree_node_data)
             father_node_id_for_next_node = name
+    tree.show()
 
 
-    tree.merge()
-    tree.show(data_property='age')
+if __name__ == '__main__':
+    result_list2 = get_root_name("penis")
+    path_result_list = get_root_name("neck_brace")
+    tree = Tree()
+    build_tree(tree, path_result_list, '../123455')
+    build_tree(tree, result_list2, '../123455')
